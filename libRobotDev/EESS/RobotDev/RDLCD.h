12ba8a86e1071627f81382cfdbcb3288aad871a5
Jerrold Luck <Jerrold.Luck@gmail.com>
2014-09-18 20:52:34 +1000

    Updated cheatsheet, header files, removed ATS project files.

--- libRobotDev/EESS/RobotDev/RDLCD.h
+++ libRobotDev/EESS/RobotDev/RDLCD.h
@@ -2,7 +2,7 @@
  * RobotDevLCD.h
  * Purpose: Provides LCD functions
  * Created: 27/07/2014
- * Author(s): Jerry Luck, Jeremy Pearson
+ * Author(s): Jerry Luck
  * Status: TESTED
  */ 
 
@@ -39,12 +39,12 @@
 #define CPU_8MHz        0x01
 
 // Helpful Macros
-#define SET_INPUT(portdir, pin)			((portdir) &= ~(1 << (pin)))
-#define SET_OUTPUT(portdir, pin)		((portdir) |= (1 << (pin)))
-#define OUTPUT_WRITE(port, pin, value)	((port) = ((port) & ~(1 << (pin))) | ((value) << (pin)))
-#define OUTPUT_LOW(port, pin)			((port) &= ~(1 << (pin)))
-#define OUTPUT_HIGH(port, pin)			((port) |= (1 << (pin)))
-#define READ_LINE(port, pin)			((port) & (1 << (pin)))
+#define SET_INPUT(portdir, pin)			portdir &= ~(1 << pin) 
+#define SET_OUTPUT(portdir, pin)		portdir |= (1 << pin)
+#define OUTPUT_WRITE(port, pin, value)	port = (port & ~(1 << pin)) | (value << pin)
+#define OUTPUT_LOW(port, pin)			port &= ~(1 << pin)
+#define OUTPUT_HIGH(port, pin)			port |= (1 << pin)
+#define READ_LINE(port, pin)			port & (1 << pin)
 
 void RDSPIInit(void) {
 	// Set MOSI and SCK as outputs for the SPI module
